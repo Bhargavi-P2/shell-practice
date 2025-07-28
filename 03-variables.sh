5ba7aeef0a9c828b8ef6c020b5694ded8b3b40c5
Bhargavi-P2 <bhanu042p@gmail.com>
2025-07-28 22:06:50 +0530

    variables started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=trump
+PERSON2=musk
+
+
+echo "$PERSON1: Hi $PERSON2 how are you"
+echo "$PERSON2: hello $PERSON1, I'm good how are you"
+echo "$PERSON1: what are you doing"
+echo "$PERSON2: going to Mars"
