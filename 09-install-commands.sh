a8fdd868da2f166967dcdb504a560a1d3b6ed797
Bhargavi-P2 <bhanu042p@gmail.com>
2025-07-29 20:22:04 +0530

    install commands

--- 09-install-commands.sh
+++ 09-install-commands.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error:: Please rin this script with root access"
+    echo "Error:: Please run this script with root access"
     exit 1  # give other than 0 upto 127
 else
     echo "You are running with root access"
@@ -16,6 +16,6 @@ then
     echo "Installing MYSQL is ... SUCCESS"
 else 
     echo " Installing MYSQL is .. FAILURE"
-    #exit 1
+    exit 1
 fi    
 
